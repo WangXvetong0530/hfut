be775fb49e9813514b5fc93648019f8373a3c69d
wxt <19339187048@163.com>
2023-10-26 09:15:18 +0800

    second commit

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    cout << "你好" << endl;
+    
+    system("pause");
+    return 0;
+
+}
